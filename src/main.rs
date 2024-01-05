192695bf632ac0d6f3daf85c24be6a1c1bfed269
Vasant Hegde <hegdevasanth1995@gmail.com>
2024-01-05 23:21:38 +0530

    finish todo app 0.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,150 @@
+#[macro_use]
+extern crate prettytable;
+use colored::Colorize;
+use prettytable::Table;
 use std::{
     collections::HashMap,
     fs::{self, File},
     io::{Read, Write},
-    path::Path,
+    path::{Path, PathBuf},
 };
 
 use chrono::{Duration, Local, NaiveDate};
 use clap::{Parser, Subcommand, ValueEnum};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 
 fn main() {
-    let today = Local::now().format("%Y-%m-%d").to_string();
-    let mut path = dirs::home_dir().unwrap();
-    path.push(".lazytodo");
-    fs::create_dir_all(&path).expect("failed to create config folder");
+    let today = Local::now().date_naive();
+    let mut todo_config_path = dirs::home_dir().unwrap();
+    todo_config_path.push(".lazytodo");
+    let todo_file_path = Path::new(&todo_config_path).join("todo.json");
 
-    let json_path = Path::new(&path).join("todo.json");
-    let mut file = File::open(&json_path).expect("Unable to open file");
+    if !todo_file_path.exists() {
+        fs::create_dir_all(&todo_config_path).expect("Failed to create config folder");
+        File::create(&todo_file_path).expect("Unable to create todo.json file");
+        write_todo_wrapper(
+            TodoWrapper {
+                todo: vec![],
+                counter: 0,
+            },
+            &todo_file_path,
+        )
+    }
+
+    let mut file = File::open(&todo_file_path).expect("Unable to open todo json file");
 
     // Read the file content into a string
     let mut contents = String::new();
     file.read_to_string(&mut contents)
-        .expect("Unable to read file");
-    let mut todos: HashMap<String, Vec<Todo>> =
-        serde_json::from_str(&contents).expect("Unable to parse JSON");
+        .expect("Unable to read todo json file");
+    let mut wrapper: TodoWrapper =
+        serde_json::from_str(&contents).expect("Unable to parse todo file content");
 
-    println!("{:?}", todos);
+    let total_todos = wrapper.todo.len();
+    // clear old todos which are greater than 30 days
+    wrapper
+        .todo
+        .retain(|x| x.created_at >= today.checked_sub_signed(Duration::days(30)).unwrap());
 
-    let todo = LazyTodo::parse();
-    match todo.command.unwrap() {
+    if wrapper.todo.len() < total_todos {
+        println!("Cleared {} old todos", total_todos - wrapper.todo.len())
+    }
+    let cli = LazyTodo::parse();
+    match cli.command {
         Commands::Add { todo, priority } => {
-            let entry = todos.entry(today).or_insert(Vec::new());
-            entry.push(Todo {
-                id: 1,
+            wrapper.counter += 1;
+            wrapper.todo.push(Todo {
+                id: wrapper.counter,
                 content: todo,
                 priority: priority.unwrap_or_default(),
                 done: false,
+                created_at: today,
             })
         }
 
         Commands::List { range } => {
-            let ls: Vec<_> = todos
+            let mut group: HashMap<&NaiveDate, Vec<&Todo>> = HashMap::new();
+            let ls: Vec<_> = wrapper
+                .todo
                 .iter()
-                .filter(|x| isValidBucket(x.0.clone(), range.unwrap_or(Bucket::ALL)))
-                .flat_map(|x| x.1)
+                .filter(|x| is_valid_bucket(x.created_at.clone(), range.unwrap_or(Bucket::ALL)))
                 .collect();
-            println!("{:?}", ls)
+
+            for todo in ls {
+                let entry = group.entry(&todo.created_at).or_insert(Vec::new());
+                entry.push(todo);
+            }
+
+            let mut table = Table::new();
+            table.add_row(row![
+                "ID".bold().cyan(),
+                "TODO_NAME".bold().cyan(),
+                "CREATED_AT".bold().cyan()
+            ]);
+
+            for todos_grouped in group.iter().sorted() {
+                for todo_item in todos_grouped.1 {
+                    let content = if todo_item.done && todo_item.priority {
+                        todo_item
+                            .content
+                            .to_uppercase()
+                            .strikethrough()
+                            .italic()
+                            .dimmed()
+                            .red()
+                    } else if todo_item.done {
+                        todo_item
+                            .content
+                            .to_uppercase()
+                            .strikethrough()
+                            .italic()
+                            .dimmed()
+                    } else if todo_item.priority {
+                        todo_item.content.to_uppercase().red()
+                    } else {
+                        todo_item.content.to_uppercase().normal()
+                    };
+                    table.add_row(row![todo_item.id, content, todo_item.created_at]);
+                }
+            }
+
+            table.printstd();
         }
-        Commands::Done { id } => {}
-        Commands::Pset { id } => {}
-        Commands::Clear { range } => {}
+
+        Commands::Done { id } => {
+            if let Some(todo) = wrapper.todo.iter_mut().find(|x| x.id == id) {
+                todo.done = true
+            }
+        }
+        Commands::Pset { id } => {
+            if let Some(todo) = wrapper.todo.iter_mut().find(|x| x.id == id) {
+                todo.priority = true
+            }
+        }
+        Commands::Clear { range } => wrapper
+            .todo
+            .retain(|x| !is_valid_bucket(x.created_at, range)),
+        Commands::Delete { id } => wrapper.todo.retain(|x| x.id != id),
     }
 
-    let json_string = serde_json::to_string(&todos).expect("Failed to serialize HashMap to JSON");
-    let mut file = File::create(json_path).expect("Failed to create file");
+    write_todo_wrapper(wrapper, &todo_file_path)
+}
+
+fn write_todo_wrapper(todo_wrapper: TodoWrapper, todo_file_path: &PathBuf) {
+    let json_string = serde_json::to_string(&todo_wrapper).expect("Failed to deserialize to JSON");
+    let mut file = File::create(todo_file_path).expect("Failed to create file");
     file.write_all(json_string.as_bytes())
         .expect("Failed to write to file");
 }
-
-fn isValidBucket(key: String, bucket: Bucket) -> bool {
+fn is_valid_bucket(key: NaiveDate, bucket: Bucket) -> bool {
     let today = Local::now().naive_local().date();
-    let key_date = NaiveDate::parse_from_str(&key, "%Y-%m-%d").expect("Failed to parse key date");
 
     match bucket {
-        Bucket::TODAY => return today == key_date,
-        Bucket::WEEK => return key_date >= today.checked_sub_signed(Duration::days(7)).unwrap(),
-        Bucket::MONTH => return key_date >= today.checked_sub_signed(Duration::days(30)).unwrap(),
-        _ => return false,
+        Bucket::TODAY => return today == key,
+        Bucket::WEEK => return key >= today.checked_sub_signed(Duration::days(7)).unwrap(),
+        Bucket::MONTH => return key >= today.checked_sub_signed(Duration::days(30)).unwrap(),
+        Bucket::ALL => return true,
     }
 }
 
@@ -74,7 +152,7 @@ fn isValidBucket(key: String, bucket: Bucket) -> bool {
 #[command(author, version, about, long_about = None)]
 struct LazyTodo {
     #[command(subcommand)]
-    command: Option<Commands>,
+    command: Commands,
 }
 
 #[derive(Subcommand)]
@@ -96,6 +174,9 @@ enum Commands {
     Pset {
         id: i32,
     },
+    Delete {
+        id: i32,
+    },
 }
 
 #[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
@@ -106,10 +187,17 @@ enum Bucket {
     ALL,
 }
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Deserialize, Serialize, PartialEq, Eq, PartialOrd, Ord)]
 struct Todo {
     id: i32,
     content: String,
     priority: bool,
     done: bool,
+    created_at: NaiveDate,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TodoWrapper {
+    todo: Vec<Todo>,
+    counter: i32,
 }
